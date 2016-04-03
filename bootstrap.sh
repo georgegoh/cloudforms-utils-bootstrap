09da8beffe7660a5156736132c6738d83326f7b8
George Goh <george.goh@redhat.com>
2016-04-03 19:49:12 +0800

    Updated cfme-consulting-scripts rpm version.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ echo "Installing CloudForms Utils"
 rpm -Uvh https://packagecloud.io/georgegoh/cloudforms-util/el/7/noarch/cloudforms-util-0.3-4.noarch.rpm
 
 echo "Installing RH consulting scripts"
-rpm -Uvh https://packagecloud.io/georgegoh/cfme-rhconsulting-scripts/el/7/noarch/cfme-rhconsulting-scripts-0.3-2.noarch.rpm
+rpm -Uvh https://packagecloud.io/georgegoh/cfme-rhconsulting-scripts/el/7/noarch/cfme-rhconsulting-scripts-0.4-1.noarch.rpm
 
 echo "Updating environment"
 source /etc/profile
